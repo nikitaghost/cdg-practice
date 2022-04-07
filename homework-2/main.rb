1f7cdd18f73a49a78f3edb905ffb01bb22a8e649
nikitaghost <nekhd@yandex.ru>
2022-04-07 14:14:39 +0000

    refactor main.rb

--- homework-2/main.rb
+++ homework-2/main.rb
@@ -1,26 +1,26 @@
 def check_word(word)  
-    word[-2..-1] == "CS" ? (p (2 ** word.length)) : (p (word.reverse))
+  word[-2..-1] == "CS" ? (p (2 ** word.length)) : (p (word.reverse))
+end
+  
+def create_pokemons_array(number_of_pokemons)
+  pokemons = []
+
+  number_of_pokemons.times do
+    print ("Enter name of pokemon ")
+    name = gets.chomp
+    print ("Enter color of pokemon ")
+    color = gets.chomp
+    pokemons << { name => color }
   end
+
+  pokemons
+end
   
-  def create_pokemons_array(number_of_pokemons)
-    pokemons = []
-    
-    number_of_pokemons.times do
-      print ("Enter name of pokemon ")
-      name = gets.chomp
-      print ("Enter color of pokemon ")
-      color = gets.chomp
-      pokemons << { name => color }
-    end
-    
-    pokemons
-  end
+#   print ("Enter random word: ")
+#   word = gets.chomp
+#   puts check_word(word)
   
-  print ("Enter random word: ")
-  word = gets.chomp
-  puts check_word(word)
+#   print ("How many pokemons create? ")
+#   number_of_pokemons = gets.to_i
   
-  print ("How many pokemons create? ")
-  number_of_pokemons = gets.to_i
-  
-  puts create_pokemons_array(number_of_pokemons)
+#   puts create_pokemons_array(number_of_pokemons)
